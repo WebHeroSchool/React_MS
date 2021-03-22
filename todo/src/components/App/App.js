6f1966d1a55443f6a685d78e26c1e44cce4fa485
MariyaShulyak <shulyakmariya@yandex.ru>
2021-03-22 16:45:27 +0300

    forms

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -23,7 +23,8 @@ class App extends React.Component {
         id:3
       }
     ],
-    count: 6
+    count: 3,
+    formError:false
   };
 
   onClickDone = id => {
@@ -45,16 +46,39 @@ class App extends React.Component {
     this.setState({items: newItemList});
   };
 
+  onClickAdd = value => {
+  		if ( value !== '') {
+
+  			this.setState(state => ({
+  				items: [
+  					...state.items,
+  					{
+  						value,
+  						isDone: false,
+  						id: state.count + 1
+  					}
+  				],
+  				count: state.count + 1,
+  				formError: false
+  			}));
+  	} else {
+  		this.setState(state => ({
+  			formError: true
+  		}))
+    }
+}
   render () {
     return (
       <div className={styles.wrap}>
-      <h1 className={styles.title}>Список важных дел:</h1>
-      <InputCase />
-      <ItemList
-      items ={this.state.items}
-      onClickDone={this.onClickDone}
-      onClickDelete={this.onClickDelete}  />
-      <Footer count = {this.state.count} />
+        <h1 className={styles.title}>Список важных дел:</h1>
+        <InputCase
+        onClickAdd = {this.onClickAdd}
+        formError= {this.state.formError} />
+        <ItemList
+        items ={this.state.items}
+        onClickDone={this.onClickDone}
+        onClickDelete={this.onClickDelete}  />
+        <Footer count = {this.state.count} />
       </div>);
   }
 };
--- todo/src/components/InputCase/InputCase.js
+++ todo/src/components/InputCase/InputCase.js
@@ -1,18 +1,61 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
+class InputCase extends React.Component {
+  state = {
+    inputValue:''
+  };
+  onButtonClick = () => {
+    this.setState ({
+      inputValue: ''
+    });
+    this.props.onClickAdd (this.state.inputValue);
+  };
 
-const InputCase = () => (
-  <TextField
-   id="standard-full-width"
-   style={{ margin: 8 }}
-   placeholder="Введите текст"
-   helperText="Добавьте Ваше задание"
-   fullWidth
-   margin="normal"
-   InputLabelProps={{
-     shrink: true,
-   }} />
-);
+  render () {
+    const {onClickAdd} = this.props;
+    const condition = this.props.formError;
+		let formHelperText;
+    if (condition) {
+      formHelperText = <FormHelperText id="component-error-text">
+        Необходимо ввести текст
+        </FormHelperText>}
+     else {
+      formHelperText = ''
+    };
+
+    return (
+      <>
+      <FormControl error>
+      <TextField
+       id="standard-full-width"
+       style={{ margin: 8 }}
+       placeholder="Добавить задание"
+       fullWidth
+       margin="normal"
+       InputLabelProps={{
+       shrink: true,
+       }}
+       value = {this.state.inputValue.toUpperCase()}
+       onChange ={event => this.setState({inputValue:event.target.value})}
+        />
+        <>
+        {formHelperText}
+        </>
+        </FormControl>
+      <Button
+      variant="contained"
+      color="primary"
+      fullWidth
+      className = "btn"
+      onClick = {this.onButtonClick}>
+          Добавить задание
+     </ Button>
+   </>);
+ }
+}
 
 export default InputCase;
